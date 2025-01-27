3da540a08641fa6ca0cff0d8fab3320d6cf2976a
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2025-01-28 03:34:14 +0530

    Update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,6 @@ set -o errexit
 set -o errtrace
 IFS=$'\n\t'
 
-export S3_BUCKET=${S3_BUCKET:-}
-export MOUNT_POINT=${MOUNT_POINT:-}
-
 mkdir -p /mnt/$S3_BUCKET
 
 s3-mount -f $S3_BUCKET /mnt/$S3_BUCKET --allow-other
